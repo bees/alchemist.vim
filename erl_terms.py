29598f1eddc7243c372b85b91aba48912996b604
bees <ariandaneshvar@gmail.com>
2017-11-14 11:10:20 -0600

    update erl_terms for python3 compatibility
    note: not compatible with python2. Work for a future commit

--- erl_terms.py
+++ erl_terms.py
@@ -1,7 +1,7 @@
 import struct
 #def encode(py_struct):
 
-FORMAT_VERSION = '\x83' #struct.pack("b", 131)
+FORMAT_VERSION = b'\x83' #struct.pack("b", 131)
 
 NEW_FLOAT_EXT = 70      # [Float64:IEEE float]
 BIT_BINARY_EXT = 77     # [UInt32:Len, UInt8:Bits, Len:Data]
@@ -30,9 +30,9 @@ COMPRESSED = 80         # [UInt4:UncompressedSize, N:ZlibCompressedData]
 
 def decode(binary):
     """
-        >>> decode('\\x83' + SMALL_INTEGER_EXT + '\x01')
+        >>> decode(b'\\x83' + SMALL_INTEGER_EXT + b'\\x01')
         1
-        >>> decode('\\x83\\x74\\x00\\x00\\x00\\x01\\x64\\x00\\x05\\x65\\x72\\x72\\x6F\\x72\\x64\\x00\\x03\\x6E\\x69\\x6c')
+        >>> decode(b'\\x83\\x74\\x00\\x00\\x00\\x01\\x64\\x00\\x05\\x65\\x72\\x72\\x6F\\x72\\x64\\x00\\x03\\x6E\\x69\\x6c')
         {'error': None}
         >>> decode(encode(-256))
         -256
@@ -49,10 +49,10 @@ def decode(binary):
         [1]
         >>> decode(encode(['a']))
         ['a']
-        >>> decode(encode({'error': None, 'payload': {'active_param': 1, 'pipe_before': False}, 'signatures': [{'docs': 'docs', 'name': 'name', 'params': ['list']}, {'docs': 'snd doc', 'params': ['list']}], 'request_id': 1 }))
-        {'error': None, 'signatures': [{'docs': 'docs', 'params': ['list'], 'name': 'name'}, {'docs': 'snd doc', 'params': ['list']}], 'payload': {'active_param': 1, 'pipe_before': False}, 'request_id': 1}
+        >>> decode(encode({'error': None, 'payload': {'active_param': 1, 'pipe_before': False}, 'signatures': [{'docs': 'docs', 'name': 'name', 'params': ['list']}, {'docs': 'snd doc', 'params': ['list']}], 'request_id': 1}))
+        {'error': None, 'payload': {'active_param': 1, 'pipe_before': False}, 'signatures': [{'docs': 'docs', 'name': 'name', 'params': ['list']}, {'docs': 'snd doc', 'params': ['list']}], 'request_id': 1}
     """
-    if binary[0] != FORMAT_VERSION:
+    if not bytes.startswith(binary, FORMAT_VERSION):
         raise NotImplementedError("Unable to serialize version %s" % binary[0])
     binary = binary[1:]
 
@@ -60,32 +60,32 @@ def decode(binary):
     return fn(binary[0: obj_size])
 
 def __decode_func(binary):
-    if binary[0] == SMALL_INTEGER_EXT:
+    if bytes.startswith(binary, SMALL_INTEGER_EXT):
         return (2, __decode_int)
-    elif binary[0] == INTEGER_EXT:
+    elif bytes.startswith(binary, INTEGER_EXT):
         return (5, __decode_int)
-    elif binary[0] == BINARY_EXT:
+    elif bytes.startswith(binary, BINARY_EXT):
         (size, ) = struct.unpack(">L", binary[1:5])
         return (1 + 4 + size, __decode_string)
-    elif binary[0] == ATOM_EXT:
+    elif bytes.startswith(binary, ATOM_EXT):
         (size, ) = struct.unpack(">H", binary[1:3])
         return (1 + 2 + size, __decode_atom)
-    elif binary[0] == NIL_EXT:
+    elif bytes.startswith(binary, NIL_EXT):
         return (1, __decode_list)
-    elif binary[0] == LIST_EXT:
+    elif bytes.startswith(binary, LIST_EXT):
         (list_size, ) = struct.unpack(">L", binary[1:5])
         tmp_binary = binary[5:]
         byte_size = 0
-        for i in xrange(list_size):
+        for i in range(list_size):
             (obj_size, fn) = __decode_func(tmp_binary)
             byte_size = byte_size + obj_size
             tmp_binary = tmp_binary[obj_size:]
         return (1 + 4 + byte_size + 1, __decode_list)
-    elif binary[0] == MAP_EXT:
+    elif bytes.startswith(binary, MAP_EXT):
         (map_size, ) = struct.unpack(">L", binary[1:5])
         tmp_binary = binary[5:]
         byte_size = 0
-        for i in xrange(map_size):
+        for i in range(map_size):
             (obj_size, fn) = __decode_func(tmp_binary)
             byte_size = byte_size + obj_size
             tmp_binary = tmp_binary[obj_size:]
@@ -107,12 +107,12 @@ def __decode_map(binary):
         >>> __decode_map(__encode_map({'foo': {'bar': 4938}}))
         {'foo': {'bar': 4938}}
         >>> __decode_map(__encode_map({'error': None, 'payload': {'active_param': 1, 'pipe_before': False}, 'signatures': [{'docs': 'docs', 'name': 'name', 'params': ['list']}, {'docs': 'snd doc', 'params': ['list']}], 'request_id': 1 }))
-        {'error': None, 'signatures': [{'docs': 'docs', 'params': ['list'], 'name': 'name'}, {'docs': 'snd doc', 'params': ['list']}], 'payload': {'active_param': 1, 'pipe_before': False}, 'request_id': 1}
+        {'error': None, 'payload': {'active_param': 1, 'pipe_before': False}, 'signatures': [{'docs': 'docs', 'name': 'name', 'params': ['list']}, {'docs': 'snd doc', 'params': ['list']}], 'request_id': 1}
     """
     (size,) = struct.unpack(">L", binary[1:5])
     result = {}
     binary = binary[5:]
-    for i in xrange(size):
+    for i in range(size):
 
         (key_obj_size, key_fn) = __decode_func(binary)
         key = key_fn(binary[0: key_obj_size])
@@ -145,7 +145,7 @@ def __decode_list(binary):
     (size, ) = struct.unpack(">L", binary[1:5])
     result = []
     binary = binary[5:]
-    for i in xrange(size):
+    for i in range(size):
         (obj_size, fn) = __decode_func(binary)
         result.append(fn(binary[0: obj_size]))
         binary = binary[obj_size:]
@@ -157,7 +157,7 @@ def __decode_string(binary):
         >>> __decode_string(__encode_string("h"))
         'h'
     """
-    return binary[5:]
+    return bytes.decode(binary[5:])
 
 def __decode_atom(binary):
     """
@@ -169,7 +169,7 @@ def __decode_atom(binary):
         >>> __decode_atom(__encode_atom("my_key"))
         'my_key'
     """
-    atom = binary[3:]
+    atom = binary[3:].decode()
     if atom == 'true':
         return True
     elif atom == 'false':
@@ -185,49 +185,49 @@ def __decode_int(binary):
         >>> __decode_int(__encode_int(256))
         256
     """
-    if binary[0] == 'a' :
-        (num,) = struct.unpack("B", binary[1])
+    if bytes.startswith(binary, SMALL_INTEGER_EXT):
+        num = int.from_bytes(binary[1:], byteorder='big')
         return num
-    (num,) = struct.unpack(">l", binary[1:])
+    num = int.from_bytes(binary[1:], byteorder='big', signed=True)
     return num
 
 def encode(struct):
     """
         >>> encode(False)
-        '\\x83d\\x00\\x05false'
+        b'\\x83d\\x00\\x05false'
         >>> encode([])
-        '\\x83j'
+        b'\\x83j'
     """
     return FORMAT_VERSION + __encoder_func(struct)(struct)
 
 def __encode_list(obj):
     """
         >>> __encode_list([])
-        'j'
+        b'j'
         >>> __encode_list(['a'])
-        'l\\x00\\x00\\x00\\x01m\\x00\\x00\\x00\\x01aj'
+        b'l\\x00\\x00\\x00\\x01m\\x00\\x00\\x00\\x01aj'
         >>> __encode_list([1])
-        'l\\x00\\x00\\x00\\x01a\\x01j'
+        b'l\\x00\\x00\\x00\\x01a\\x01j'
     """
     if len(obj) == 0:
         return NIL_EXT
     b = struct.pack(">L", len(obj))
     for i in obj:
-        b = '%s%s' %(b, __encoder_func(i)(i))
+        b = b'%s%s' %(b, __encoder_func(i)(i))
     return LIST_EXT + b + NIL_EXT
 
 def __encode_map(obj):
     """
         >>> __encode_map({'foo': 1})
-        't\\x00\\x00\\x00\\x01m\\x00\\x00\\x00\\x03fooa\\x01'
+        b't\\x00\\x00\\x00\\x01m\\x00\\x00\\x00\\x03fooa\\x01'
         >>> __encode_map({'foo': 'bar'})
-        't\\x00\\x00\\x00\\x01m\\x00\\x00\\x00\\x03foom\\x00\\x00\\x00\\x03bar'
+        b't\\x00\\x00\\x00\\x01m\\x00\\x00\\x00\\x03foom\\x00\\x00\\x00\\x03bar'
         >>> __encode_map({'foo': {'bar': 4938}})
-        't\\x00\\x00\\x00\\x01m\\x00\\x00\\x00\\x03foot\\x00\\x00\\x00\\x01m\\x00\\x00\\x00\\x03barb\\x00\\x00\\x13J'
+        b't\\x00\\x00\\x00\\x01m\\x00\\x00\\x00\\x03foot\\x00\\x00\\x00\\x01m\\x00\\x00\\x00\\x03barb\\x00\\x00\\x13J'
     """
     b = struct.pack(">L", len(obj))
-    for k,v in obj.iteritems():
-        b = '%s%s%s' % (b, __encoder_func(k)(k), __encoder_func(v)(v))
+    for k,v in obj.items():
+        b = b'%s%s%s' % (b, __encoder_func(k)(k), __encoder_func(v)(v))
     return MAP_EXT + b
 
 def __encoder_func(obj):
@@ -249,25 +249,25 @@ def __encoder_func(obj):
 def __encode_string(obj):
     """
         >>> __encode_string("h")
-        'm\\x00\\x00\\x00\\x01h'
+        b'm\\x00\\x00\\x00\\x01h'
         >>> __encode_string("hello world!")
-        'm\\x00\\x00\\x00\\x0chello world!'
+        b'm\\x00\\x00\\x00\\x0chello world!'
     """
-    return BINARY_EXT + struct.pack(">L", len(obj)) + obj
+    return BINARY_EXT + struct.pack(">L", len(obj)) + str.encode(obj)
 
 def __encode_none(obj):
     """
         >>> __encode_none(None)
-        'd\\x00\\x03nil'
+        b'd\\x00\\x03nil'
     """
     return __encode_atom("nil")
 
 def __encode_boolean(obj):
     """
         >>> __encode_boolean(True)
-        'd\\x00\\x04true'
+        b'd\\x00\\x04true'
         >>> __encode_boolean(False)
-        'd\\x00\\x05false'
+        b'd\\x00\\x05false'
     """
     if obj == True:
         return __encode_atom("true")
@@ -277,19 +277,19 @@ def __encode_boolean(obj):
         raise "Maybe later"
 
 def __encode_atom(obj):
-    return ATOM_EXT + struct.pack(">H", len(obj)) + (b"%s" % obj)
+    return ATOM_EXT + struct.pack(">H", len(obj)) + (str.encode(obj))
 
 def __encode_int(obj):
     """
         >>> __encode_int(1)
-        'a\\x01'
+        b'a\\x01'
         >>> __encode_int(256)
-        'b\\x00\\x00\\x01\\x00'
+        b'b\\x00\\x00\\x01\\x00'
     """
     if 0 <= obj <= 255:
-        return SMALL_INTEGER_EXT +  struct.pack("B", obj)
+        return SMALL_INTEGER_EXT + int.to_bytes(obj, 1, byteorder='big')
     elif -2147483648 <= obj <= 2147483647:
-        return INTEGER_EXT + struct.pack(">l", obj)
+        return INTEGER_EXT + int.to_bytes(obj, 4, byteorder='big', signed=True)
     else:
         raise "Maybe later"
 if __name__ == "__main__":
